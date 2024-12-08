4b09c97bd535c082fc91c75a44c6167b9c867d4f
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-08 19:46:29 -0300

    Adicionado função de exibir os livros na tela, buscando as informações da requisição

--- app/main.js
+++ app/main.js
@@ -1,9 +1,32 @@
 let livros = [];
 const endPointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
-getBuscarLivrosAPI();
+fGetBuscarLivrosAPI();
+const elementosInserirLivros = document.getElementById('livros');
 
-async function getBuscarLivrosAPI() {
+
+async function fGetBuscarLivrosAPI() {
     const res = await fetch(endPointAPI);
     livros = await res.json();
     console.table(livros);
+    fExibirOsLivrosNaTela(livros);
+}
+
+
+function fExibirOsLivrosNaTela(listaLivros) {
+    listaLivros.forEach(livro => {
+        elementosInserirLivros.innerHTML += `
+            <div class="livro">
+            <img class="livro__imagens" src="${livro.imagem}"
+            alt"${livro.alt}" />
+            <h2 class="livro__titulo">
+                ${livro.titulo}
+            </h2>
+            <p class="livro__descricao">${livro.autor}</p>
+            <p class="livro__preco" id="preco">${livro.preco}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
+            </div>
+        `
+    });
 }
