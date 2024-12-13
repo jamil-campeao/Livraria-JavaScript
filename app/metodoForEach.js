28167e612589d07fa4ad77edd39b81d63a5a0441
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-13 01:20:28 -0300

    Criado função para apresentar a disponibilidade de livros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,9 +4,10 @@ const elementosInserirLivros = document.getElementById('livros');
 function fExibirOsLivrosNaTela(listaLivros) {
     elementosInserirLivros.innerHTML = '';
     listaLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro_imagens' : 'livros_imagens indisponivel'; 
         elementosInserirLivros.innerHTML += `
             <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}"
+            <img class="${disponibilidade}" src="${livro.imagem}"
             alt"${livro.alt}" />
             <h2 class="livro__titulo">
                 ${livro.titulo}
