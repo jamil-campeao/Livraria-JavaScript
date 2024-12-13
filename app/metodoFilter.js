d3993a522400db55a90544dcc91dcaf632d6de27
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-13 01:29:27 -0300

    Adicionado função para listar livros disponíveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,8 @@ botoes.forEach(btn => btn.addEventListener('click', fFiltrarLivros));
 function fFiltrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter
+    (livro => livro.quantidade > 0) : livros.filter(livro => livro.
+        categoria == categoria);
     fExibirOsLivrosNaTela(livrosFiltrados);
 }
